d6b3871889e51a0a0e67b4a5f52d40507d1b8330
DaviCM <davimaciel2008@gmail.com>
2025-08-07 12:26:42 -0300

    Deu tudo errado

--- aula1/variaveis.py
+++ aula1/variaveis.py
@@ -1,1 +1,20 @@
-print("Olá, mundo!")
+operations = ['Insira seu nome: ', 'Insira sua idade: ', 'Insira sua data de nascimento no formato DD/MM/AAAA: ']
+values = []
+
+for i in range(3):
+    values.append(input(operations[i]).strip())
+    if len(values) == 3:
+        break
+print(values)
+    
+while i == True:    
+    try:
+        values[1] = int(values[1])
+        i == False
+    except ValueError:
+       values[2] = print('Esse valor para a idade não é inteiro. Insira outro: ')
+
+
+print(f'Seu nome é {values[0]}, você tem {values[1]} anos e nasceu em {values[2]}!')
+
+
