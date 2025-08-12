d97350a1d135624367417f90fbb94f9ffa34a638
DaviCM <davimaciel2008@gmail.com>
2025-08-12 07:57:18 -0300

    .

--- aula1/tedio.py
+++ aula1/tedio.py
@@ -2,12 +2,16 @@ from time import sleep
 
 n = 1
 
+def LaEle():
+    if '24' in str(n):
+        print(f'Lá ele! O valor é {n}!')
+
+
 while True:
     print(f'O valor é: {n}')
+    LaEle()
     n += 1
     sleep(0.05)
     
-    if '24' in str(n):
-        print(f'Lá ele! O valor é {n}!')
-        n += 1
-        
+
+    
