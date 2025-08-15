bb292f0abf1ae69bb012f0bb44ffe28f4c802b9f
DaviCM <davimaciel2008@gmail.com>
2025-08-14 21:58:34 -0300

    Que sistema horroroso

---
+++ aula3/cinema.py
@@ -0,0 +1,49 @@
+from time import sleep
+from os import system
+
+sessions = [[1, 0, 'Girls und Panzer - Das Finale'], [2, 10, 'Herobrine - A Lenda'], [3, 12, 'Guerra nas IDEs - O Retorno de Java'],
+            [4, 14, 'Carro Pika vs Celta 2012'], [5, 16, 'Velozes e Furiosos - Desafio em Ceilândia'], [6, 18, 'Red Dead Redemption']]
+
+quit = False
+esc = ''
+
+while quit == False:
+    print('\n'f'{8 * '-'} Bem vindo ao CINEMÁTICO! {8 * '-'}')
+    if esc == '':
+        nome = input('Insira seu nome: ').capitalize()
+        
+        try:
+            id_cliente = int(input('Insira sua idade: '))
+        except ValueError:
+            print('Valor inválido para a idade. Reiniciando programa.')
+            continue
+    else:
+        pass
+    
+    for i in range(6):
+        print(f'Sala {sessions[i][0]}: {sessions[i][2]}')
+        print(f'Classificação indicativa: {sessions[i][1]} anos.''\n')
+        sleep(0.2)
+ 
+    esc = (int(input('Escola a sala com o filme que quer assistir: ')))
+    system('cls')
+    
+    if esc >= 1 <= 6:
+
+        if id_cliente < sessions[esc - 1][1]:
+            print('Você não pode assistir ao filme. Escolha um com a classificação adequada.')
+            sleep(1)
+            continue
+            
+        else:
+            print('\n'f'{8 * '-'} Dados da sessão: {8 * '-'} ')
+            print(f'Nome: {nome}')
+            print(f'Sala: {sessions[esc - 1][0]}')
+            print(f'Filme: {sessions[esc - 1][2]}''\n')
+            print(f'Bom filme!')
+            quit = True
+        
+    else:
+        print('Valor inválido. Reiniciando programa.')
+        continue
+    
