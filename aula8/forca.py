3bd0cb2cc4682b977ed3bcf0f82d6d66dc064832
DaviCM <davimaciel2008@gmail.com>
2025-09-04 23:10:44 -0300

    .

--- aula8/forca.py
+++ aula8/forca.py
@@ -1,63 +1,132 @@
 from random import choice
 from time import sleep
 from os import system
+import json
 
 def palavraParaAchar():
-    opts = ['python', 'developer', 'programação', 'inteligência', 'artificial', 'computador',
-            'java', 'javascript', 'garoto', 'programa', 'notebook', 'computador', 'memória',
-            'sistema', 'operacional', 'monitor', 'samsung', 'palavras', 'escola', 'anime',
-            'forca', 'gabinete', 'placa', 'teclado', 'calvície', 'laptop', 'desktop', 'hardware']
+    def getInt(value):
+        while True:
+            try:
+                retvalue = value
+                retvalue = int(input(retvalue).strip())
+                system('cls')
+
+                if (retvalue < 1) or (retvalue > i + 2):
+                    raise ValueError
+                else:
+                    return retvalue
+
+            except ValueError:
+                system('cls')
+                print('Valor inválido. Tente novamente.')
+                sleep(1)
+                system('cls')
+            
     
-    return choice(opts)
+    dictKeys = []
+    
+    with open('aula8/db.json', 'r', encoding='utf-8') as file:
+        data = (json.load(file))
+    
+    for keys in data:
+        for key in keys:
+            dictKeys.append(key)
+    
+    while True:
+        system('cls')
+        print('Bem-Vindo ao Jogo da Forca!')
+        
+        for i in range(len(dictKeys)): 
+            print(f'Opção de jogo {i + 1}: {(dictKeys[i]).title()}')
+        print(f'Opção {i + 2}: Sair do Jogo''\n')
+    
+        opt = getInt('Insira a opção desejada: ')
+
+        if opt == (i + 2):
+            system('cls')
+            print('Adeus, amigo.')
+            return False, None
+        else:
+            return True, choice(data[opt - 1].get(dictKeys[opt - 1]))
 
 
 def gameLoop():
-
-    palavraAlvo = [letra for letra in palavraParaAchar()]
-    palavraEscondida = ['_' for _ in palavraAlvo]
-    errors = []
-    tries = 6
-
-    tent = (input('Insira a letra que deseja jogar, ou tente a sorte e insira a palavra inteira: ').lower()).strip()
-
-    if len(tent) == 1 and (tent in palavraAlvo):
-        indices = []
-        # enumerate cria um par de chave e valor, que será desempacotado nas variáveis índices (para cada índice da palavra)
-        # e letra, para cada valor.
-        for indices, letra in enumerate(palavraAlvo):
-            if (letra == tent) and (tent not in palavraEscondida):
-                # Se a letra na palavra alvo for igual ao input, ele substitui o _ na palavra escondida. 
-                # Por causa do for, substituirá em todas as ocorrências
-                palavraEscondida[indices] = tent
-    elif len(tent) == 1:
-        errors.append(tent)
-        tries -= 1
-    elif (len(tent) > 1) and (tent == ''.join(palavraAlvo)):
-        system('cls')
-        print('Você ganhou, e com ousadia! Parabéns!')
-        print(f'A palavra era: {''.join(palavraAlvo)}')
-    elif len(tent) > 1:
-        system('cls')
-        print(f'Você perdeu, mas valeu a ousadia. A palavra era: {''.join(palavraAlvo)}')
-    else:
-        errors.append(tent)
-        tries -= 1
-
-    if ''.join(palavraEscondida) == ''.join(palavraAlvo):
-        system('cls')
-        print('Você ganhou! Parabéns!')
-        print(f'A palavra era: {''.join(palavraAlvo)}')
+    def mainGame():
+        valid, palavraAlvo = palavraParaAchar()
         
-    if tries == 0:
-        system('cls')
-        print(f'Você perdeu. A palavra era: {''.join(palavraAlvo)}')
+        if valid == True:
+            palavraAlvo = [letra for letra in palavraAlvo]
+            palavraEscondida = ['_' for _ in palavraAlvo]
+            errors = []
+            tries = 6
+        
+            while True:    
+                system('cls')
+                print(f'Você possui {tries} erros restantes!')
+                print(f'Sua palavra é: {' '.join(palavraEscondida)} \n')
+                print(f'Letras descartadas: {errors}' if len(errors) != 0 else 'Nenhuma letra descartada.')
+                
+                tent = (input('Insira a letra que deseja jogar, ou tente a sorte e insira a palavra inteira: ').lower()).strip()
+                
+                if len(tent) == 1 and (tent in palavraAlvo):
+                    indices = []
+                    # enumerate cria um par de chave e valor, que será desempacotado nas variáveis índices (para cada índice da palavra)
+                    # e letra, para cada valor.
+                    for indices, letra in enumerate(palavraAlvo):
+                        if (letra == tent):
+                            # Se a letra na palavra alvo for igual ao input, ele substitui o _ na palavra escondida. 
+                            # Por causa do for, substituirá em todas as ocorrências
+                            palavraEscondida[indices] = tent
+                            
+                elif len(tent) == 1:
+                    errors.append(tent) if tent not in errors else None
+                    tries -= 1
+                elif (len(tent) > 1) and (tent == ''.join(palavraAlvo)):
+                    system('cls')
+                    print('Você ganhou, e com ousadia! Parabéns!')
+                    print(f'A palavra era: {''.join(palavraAlvo)}')
+                    return True
+                elif (len(tent) > 1):
+                    system('cls')
+                    print(f'Você perdeu, mas valeu a ousadia. A palavra era: {''.join(palavraAlvo)}')
+                    return True
+                else:
+                    errors.append(tent) if tent not in errors else None
+                    tries -= 1
 
-    while True:
-        system('cls')
-        print(f'Você possui {tries} tentativas restantes!')
-        print(f'Sua palavra é: {' '.join(palavraEscondida)} \n')
-        print(f'Letras descartadas: {errors}' if len(errors) != 0 else 'Nenhuma letra descartada.')
+                if ''.join(palavraEscondida) == ''.join(palavraAlvo):
+                    system('cls')
+                    print('Você ganhou! Parabéns!')
+                    print(f'A palavra era: {''.join(palavraAlvo)}')
+                    return True
+                        
+                if tries == 0:
+                    system('cls')
+                    print(f'Você perdeu. A palavra era: {''.join(palavraAlvo)}')
+                    return True
+        else:
+            return False
+                
 
+    def postGame():
+        valid = mainGame()
+        
+        while valid == True:
+            opt = (input('\n''Deseja jogar novamente? (s/n): ').lower()).strip()
+            if opt == 's':
+                return True
+            elif opt == 'n':
+                system('cls')
+                print('Adeus amigo.')
+                return False
+            else:
+                print('Opção inválida. Tente novamente.')
+                sleep(1)
+                system('cls')
+                continue
+        else:
+            return False
+        
+    postGame()
+         
 gameLoop()
-
-
